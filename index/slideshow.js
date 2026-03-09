92871f8603d09333804ce4a3fffa06b4c16598d4
Itsihe11 <ihe@studenti.laziodigital.it>
2026-03-09 11:38:14 +0100

    dark mode

--- index/slideshow.js
+++ index/slideshow.js
@@ -33,3 +33,27 @@ document.querySelectorAll(".media-container").forEach(container => {
     });
 
 });
+
+
+const toggleButton = document.getElementById("darkModeToggle");
+
+/* Load saved mode */
+
+if(localStorage.getItem("darkMode") === "enabled"){
+document.body.classList.add("dark-mode");
+toggleButton.textContent = "☀";
+}
+
+toggleButton.addEventListener("click", () => {
+
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+localStorage.setItem("darkMode","enabled");
+toggleButton.textContent = "☀";
+}else{
+localStorage.setItem("darkMode","disabled");
+toggleButton.textContent = "☾";
+}
+
+})
