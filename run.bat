849ae01182fff28906228b509bfb1a2f4b13433d
CartMaden <emanuelecampus11@gmail.com>
2026-03-18 17:49:40 +0100

    modifica run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,17 @@
 @echo off
+
+echo Avvio Docker Desktop...
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+echo Attendo che Docker sia pronto...
+:loop
+docker info >nul 2>&1
+if %errorlevel% neq 0 (
+    timeout /t 2 >nul
+    goto loop
+)
+
+echo Docker pronto!
+
 docker compose up -d
 start http://localhost:8080
